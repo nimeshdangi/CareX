18d34c06797ed8e0753a2715945db058f1931c0e
Immanuel Bayalkoti <bayalkotiimmanuel@gmail.com>
2024-10-21 23:18:16 +0545

    profile page for doctor and patient

---
+++ src/app/profile/page.js
@@ -0,0 +1,74 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useState } from "react";
+import SideBar from "../../components/sidebar";
+import Image from "next/image";
+
+const ProfilePage = () => {
+    const router = useRouter();
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/patient/profile`, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': localStorage.getItem('token')
+                    }
+                });
+                const data = await response.json();
+                if(response.status === 401) {
+                    toast.error(data.message);
+                    router.push('/login');
+                }
+                setProfile(data.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching profile:', error);
+            }
+        };
+        fetchProfile();
+    }, [])
+
+    return (
+        <SideBar>
+            {loading ? <div className="flex justify-center items-center h-screen">Loading...</div> : (
+                <div className="w-full max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden mt-5 pb-5">
+                    <div className="flex flex-col items-center space-y-4 p-6">
+                        <div className="w-24 h-24 rounded-full overflow-hidden">
+                            <Image src="/doctor.jpg" width={200} height={200} alt="Profile Picture" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="text-center space-y-1">
+                            <h2 className="text-2xl font-bold">{profile.name}</h2>
+                            {/* <p className="text-gray-500">{profile.specification}</p> */}
+                        </div>
+                    </div>
+                    <div className="px-6 text-center">
+                        <p className="text-gray-600">
+                            <strong>Email: </strong>{profile.email}
+                        </p>
+                        <p className="text-gray-600">
+                            <strong>Contact Number: </strong>{profile.phone_number}
+                        </p>
+                        <p className="text-gray-600">
+                            <strong>Address: </strong>{profile.address}
+                        </p>
+                        {/* <p className="text-gray-600">
+                            <strong>Registration Number: </strong>{profile.registrationNumber}
+                        </p>
+                        <p>
+                            <strong>Documents: </strong>
+                            <Image src={`${process.env.NEXT_PUBLIC_SERVER_URL}/uploads/doctors/documents/${profile.documents}`} width={200} height={200} alt="Profile Picture" className="w-full h-full" />
+                        </p> */}
+                    </div>
+                </div>
+            )}
+        </SideBar>
+    )
+}
+
+export default ProfilePage;
