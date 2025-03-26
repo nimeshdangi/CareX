d3e690059c94f5440fe71fb2b4f2c9cb0285dc31
Nimesh Dangi <nimeshdangi22@gmail.com>
2025-03-26 21:40:28 +0545

    admin approves the doctor

---
+++ src/app/admin/doctors/[id]/page.js
@@ -0,0 +1,92 @@
+"use client"
+import Image from "next/image";
+import AdminSideBar from "../../../../components/adminSideBar";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+const DoctorDetails = () => {
+    const params = useParams();
+    const router = useRouter();
+    const {id} = params;
+    const [doctor, setDoctor] = useState({});
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        if(!id) {
+            router.push('/admin/doctors');
+        }
+
+        const fetchDoctor = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/doctor/${id}`);
+                const data = await response.json();
+                setDoctor(data.data);
+            } catch (error) {
+                console.error('Error fetching doctor:', error);
+            }
+        }
+
+        fetchDoctor();
+    }, [id]);
+
+    const handleDoctorApproval = async (status) => {
+        if(status === "Not Approved") {
+            console.log("Token: ", token);
+            const response = await fetch(`http://localhost:5000/doctor/approve/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": token
+                },
+            })
+
+            if(response.status === 401) {
+                router.push("/login");
+            }
+
+            if(response.ok) {
+                const data = await response.json();
+                toast.success(data.message);
+                router.push('/admin/doctors');
+            } else {
+                const data = await response.json();
+                toast.error(data.message);
+            }
+        }
+    }
+
+    return (
+        <AdminSideBar>
+            <div className="p-4">
+                <h1 className="text-3xl font-bold text-center">Doctor Details</h1>
+                <div className="w-4/5 mx-auto p-4 my-4 bg-gray-50 shadow-lg rounded-lg">
+                    {/* <div className="w-2/3 p-4"> */}
+                        <h3 className="text-2xl font-bold">Dr. {doctor.name}</h3>
+                        <p className="text-gray-500">Email: {doctor.email}</p>
+                        <p className="text-gray-500">Phone: {doctor.phone_no}</p>
+                        <p className="text-gray-500">Registration Number: {doctor.registrationNumber}</p>
+                        <p className="text-gray-500">Specialty: {doctor.specification}</p>
+                        <p className="text-gray-500">Qualification: {doctor.qualification}</p>
+                        Document
+                        <Image
+                            src={`http://localhost:5000/uploads/doctors/documents/${doctor.documents}`}
+                            alt="doctor"
+                            width={500}
+                            height={500}
+                            // className="rounded-l-lg w-1/3 object-cover"
+                        />
+                        <p className="text-gray-500">Rating: 4.5</p>
+                        <p className="text-gray-500">Experience: 10 years</p>
+                        <p className="text-gray-500">50 Appointments</p>
+                    {/* </div> */}
+                </div>
+                <button onClick={() =>handleDoctorApproval(doctor.status)} className="flex w-auto mx-auto bg-blue-500 text-white px-4 py-2 rounded-lg mt-4">
+                    {doctor.status == "Approved" ? "Decline" : "Approve"}
+                </button>
+            </div>
+        </AdminSideBar>
+    )
+}
+
+export default DoctorDetails;
--- src/app/admin/doctors/page.js
+++ src/app/admin/doctors/page.js
@@ -1,6 +1,25 @@
-import AdminSideBar from "@/components/adminSideBar";
+"use client"
+import { useEffect, useState } from "react";
+import AdminSideBar from "../../../components/adminSideBar";
+import Link from "next/link";
 
 const Doctors = () => {
+    const [doctors, setDoctors] = useState([]);
+
+    useEffect(() => {
+        const fetchDoctors = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/doctor/all');
+                const data = await response.json();
+                setDoctors(data.data);
+            } catch (error) {
+                console.error('Error fetching doctors:', error);
+            }
+        };
+
+        fetchDoctors();
+    }, []);
+
     return (
         <AdminSideBar>
             <div className="m-10">
@@ -27,7 +46,26 @@ const Doctors = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                            {doctors.map((doctor) => (
+                                <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {doctor.name}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {doctor.specification}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {doctor.registrationNumber}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {doctor.status}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <Link href={`/admin/doctors/${doctor.id}`} class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</Link>
+                                    </td>
+                                </tr>
+                            ))}
+                            {/* <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     John Doe
                                 </th>
@@ -111,7 +149,7 @@ const Doctors = () => {
                                 <td class="px-6 py-4">
                                     <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
                                 </td>
-                            </tr>
+                            </tr> */}
                         </tbody>
                     </table>
                 </div>
