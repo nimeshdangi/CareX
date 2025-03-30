607c1c748a777372e6b94c0e90e68a1699d5b907
Nimesh Dangi <nimeshdangi22@gmail.com>
2025-03-30 22:00:51 +0545

    profile for patient and doctor

---
+++ src/app/doctor/profile/page.js
@@ -0,0 +1,83 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect } from "react";
+import { useState } from "react";
+import DoctorSideBar from "../../../components/doctorSideBar";
+
+const ProfilePage = () => {
+    const [profile, setProfile] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/doctor/profile`, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': localStorage.getItem('token')
+                    }
+                });
+                const data = await response.json();
+                console.log(data);
+                setProfile(data.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching profile:', error);
+            }
+        };
+        fetchProfile();
+    }, [])
+
+    return (
+        <DoctorSideBar>
+            {loading ? <div>Loading...</div> : (
+                <div className="w-full max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden mt-5 pb-5">
+                    <div className="flex flex-col items-center space-y-4 p-6">
+                        <div className="w-24 h-24 rounded-full overflow-hidden">
+                            <Image src="/doctor.jpg" width={200} height={200} alt="Profile Picture" className="w-full h-full object-cover" />
+                        </div>
+                        <div className="text-center space-y-1">
+                            <h2 className="text-2xl font-bold">{profile.name}</h2>
+                            <p className="text-gray-500">{profile.specification}</p>
+                        </div>
+                    </div>
+                    <div className="px-6 text-center">
+                        <p className="text-gray-600">
+                            <strong>Email: </strong>{profile.email}
+                        </p>
+                        <p className="text-gray-600">
+                            <strong>Contact Number: </strong>{profile.phone_no}
+                        </p>
+                        <p className="text-gray-600">
+                            <strong>Qualification: </strong>{profile.qualification}
+                        </p>
+                        <p className="text-gray-600">
+                            <strong>Registration Number: </strong>{profile.registrationNumber}
+                        </p>
+                        <p>
+                            <strong>Documents: </strong>
+                            <Image src={`${process.env.NEXT_PUBLIC_SERVER_URL}/uploads/doctors/documents/${profile.documents}`} width={200} height={200} alt="Profile Picture" className="w-full h-full" />
+                        </p>
+                    </div>
+                    {/* <div className="flex justify-center space-x-4 p-6">
+                        <button className="text-gray-500 hover:text-gray-700">
+                        <Github className="h-5 w-5" />
+                        <span className="sr-only">GitHub</span>
+                        </button>
+                        <button className="text-gray-500 hover:text-gray-700">
+                        <Twitter className="h-5 w-5" />
+                        <span className="sr-only">Twitter</span>
+                        </button>
+                        <button className="text-gray-500 hover:text-gray-700">
+                        <Linkedin className="h-5 w-5" />
+                        <span className="sr-only">LinkedIn</span>
+                        </button>
+                    </div> */}
+                </div>
+            )}
+        </DoctorSideBar>
+    )
+}
+
+export default ProfilePage
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -73,11 +73,6 @@ const SideBar = ({children}) => {
                         </li>
                         <li>
                             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                {/* <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                                </svg> */}
                                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM6 6a1 1 0 0 1-.707-.293l-1-1a1 1 0 0 1 1.414-1.414l1 1A1 1 0 0 1 6 6Zm-2 4H3a1 1 0 0 1 0-2h1a1 1 0 1 1 0 2Zm14-4a1 1 0 0 1-.707-1.707l1-1a1 1 0 1 1 1.414 1.414l-1 1A1 1 0 0 1 18 6Zm3 4h-1a1 1 0 1 1 0-2h1a1 1 0 1 1 0 2ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z"/>
                                 </svg>
@@ -85,12 +80,7 @@ const SideBar = ({children}) => {
                             </a>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                {/* <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-                                </svg> */}
+                            <a href="/profile" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                 <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path fillRule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm10 5a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm0 3a1 1 0 0 1 1-1h3a1 1 0 1 1 0 2h-3a1 1 0 0 1-1-1Zm-8-5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm1.942 4a3 3 0 0 0-2.847 2.051l-.044.133-.004.012c-.042.126-.055.167-.042.195.006.013.02.023.038.039.032.025.08.064.146.155A1 1 0 0 0 6 17h6a1 1 0 0 0 .811-.415.713.713 0 0 1 .146-.155c.019-.016.031-.026.038-.04.014-.027 0-.068-.042-.194l-.004-.012-.044-.133A3 3 0 0 0 10.059 14H7.942Z" clipRule="evenodd"/>
                                 </svg>
@@ -113,14 +103,14 @@ const SideBar = ({children}) => {
                     </ul>
                     <ul>
                         <li>
-                            <button className="w-full py-2 px-4 mb-2 text-[#003D87] hover:text-white border border-2 border-[#003D87] hover:border-transparent hover:bg-blue-700 transition">
-                                <Link href={'/login'}>Login</Link>
-                            </button>
+                            <Link href={'/login'}>
+                                <button className="w-full py-2 px-4 mb-2 text-[#003D87] hover:text-white border border-2 border-[#003D87] hover:border-transparent hover:bg-blue-700 transition">Login</button>
+                            </Link>
                         </li>
                         <li>
-                            <button className="w-full py-2 px-4 mb-2 hover:text-[#003D87] text-white border border-2 border-transparent hover:border-[#003D87] bg-blue-700 hover:bg-transparent transition">
-                                <Link href={'/register'}>Sign Up Free</Link>
-                            </button>
+                            <Link href={'/register'}>
+                                <button className="w-full py-2 px-4 mb-2 hover:text-[#003D87] text-white border border-2 border-transparent hover:border-[#003D87] bg-blue-700 hover:bg-transparent transition">Sign Up Free</button>
+                            </Link>
                         </li>
                         {/* <li>
                             <button className="w-full py-2 px-4 text-white bg-green-600 rounded-lg hover:bg-green-700 transition">
