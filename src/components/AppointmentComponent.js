d76cc8e9f3b039b408f7da10702a016661aa446b
Nimesh Dangi <nimeshdang22@gmail.com>
2025-05-31 16:32:50 +0545

    small fix

--- src/components/AppointmentComponent.js
+++ src/components/AppointmentComponent.js
@@ -316,7 +316,7 @@ const AppointmentComponent = () => {
                                         {appointment.status === "Completed" ? (
                                             <span className="font-medium text-blue-600 dark:text-blue-500">
                                                 {appointment?.status == "Completed" ? (
-                                                <div onClick={() => router.push(`/appointments/${appointment.id}`)} className="font-medium bg-blue-100 py-2 rounded-md text-center text-blue-600 dark:text-blue-500 hover:bg-blue-200">View Appointment Detials</div>) : appointment?.status}
+                                                <div onClick={() => router.push(`/appointment/${appointment.id}`)} className="font-medium bg-blue-100 py-2 rounded-md text-center text-blue-600 dark:text-blue-500 hover:bg-blue-200">View Appointment Detials</div>) : appointment?.status}
                                                 {appointment.doctor.reviews.length == 0 && 
                                                     <button onClick={() => {setSelectedDoctorId(appointment.doctor.id);setIsReviewModalOpen(true);}} className="ml-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add Review</button>
                                                 }
