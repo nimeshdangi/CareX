1c476e965aff0c941b74d926a2267477e32196d8
Nimesh Dangi <nimeshdang22@gmail.com>
2025-05-31 12:19:27 +0545

    add review

--- src/components/AppointmentComponent.js
+++ src/components/AppointmentComponent.js
@@ -315,7 +315,10 @@ const AppointmentComponent = () => {
                                     <td className="px-6 py-4">
                                         {appointment.status === "Completed" ? (
                                             <span className="font-medium text-blue-600 dark:text-blue-500">
-                                                <div onClick={() => router.push(`/appointment/${appointment.id}`)} className="font-medium bg-blue-100 py-2 rounded-md text-center text-blue-600 dark:text-blue-500 hover:bg-blue-200 cursor-pointer">View Appointment Detials</div>
+                                                Completed
+                                                {appointment.doctor.reviews.length == 0 && 
+                                                    <button onClick={() => {setSelectedDoctorId(appointment.doctor.id);setIsReviewModalOpen(true);}} className="ml-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add Review</button>
+                                                }
                                             </span>
                                         ) : (
                                             <span className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{appointment.status}</span>
